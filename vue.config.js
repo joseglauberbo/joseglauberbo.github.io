fe9b72a7594000571982059eb880adba149071b0
joseglauberbo <jose.oliveira@ccc.ufcg.edu.br>
2019-12-17 10:43:45 -0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = { 
-    publicPath: process.env.NODE_ENV === 'production' ? '/my_portfolio/' : '/' 
+    publicPath: process.env.NODE_ENV === 'production' ? '/my_portfolio/dist/' : '/' 
 }
