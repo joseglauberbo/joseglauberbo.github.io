3793d616dd3ba1275028ceba36392ec50ac75ae3
joseglauberbo <jose.oliveira@ccc.ufcg.edu.br>
2020-02-19 13:26:21 -0300

    refactoring

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const port = process.env.PORT || 8080
 const app = express()
 
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, function(req, res) {
-    res.sendFile(__dirname + "/dist/index.html");
-});
+const root = require('path').join(__dirname, 'dist')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+})
+
 app.listen(port)
 
 console.log("server started...")
